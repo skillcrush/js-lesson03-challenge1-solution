3af864a6a211fbb7994acb8429a0d5c0aef2c04e
tauri_s <tauristclaire@gmail.com>
2026-09-23 16:49:32 -0700

    Migrate from CodeSandbox

---
+++ script.js
@@ -0,0 +1,17 @@
+var weekday = new Date().toLocaleString("en-US", { weekday: "long" });
+
+// console.log(weekday);
+
+if (weekday === "Monday") {
+  console.log("Let's get this week started!");
+} else if (weekday === "Tuesday") {
+  console.log("Worky work!");
+} else if (weekday === "Wednesday") {
+  console.log("Worky work!");
+} else if (weekday === "Thursday") {
+  console.log("Worky work!");
+} else if (weekday === "Friday") {
+  console.log("Weekend's almost here!");
+} else {
+  console.log("WEEKEND, YAYYY!!!");
+}
